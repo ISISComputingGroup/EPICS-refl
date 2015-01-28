8a9a91c76ee2b64b39a69e8655d482f122c97fdd
Dominic Oram <dominic.oram@stfc.ac.uk>
2015-01-28 11:14:51 +0000

    Removed file_watcher_tests.py as they are lengthy and rely exclusively on external module
    Former-commit-id: c51c63a450f8e4a79323e24f251b0761aefc3e13

--- BlockServer/test_blockserver.py
+++ BlockServer/test_blockserver.py
@@ -14,7 +14,6 @@ from test_modules.configuration_json_tests import TestConfigurationJsonConverter
 from test_modules.container_tests import TestContainersSequence
 from test_modules.config_holder_tests import TestConfigHolderSequence
 from test_modules.config_list_manager_tests import TestInactiveConfigsSequence
-from test_modules.file_watcher_tests import TestFileWatcherManager
 from test_modules.file_event_handler_tests import TestFileEventHandler
 
 DEFAULT_DIRECTORY = '..\\..\\..\\test-reports'
@@ -36,7 +35,6 @@ if __name__ == '__main__':
     config_holder_suite = unittest.TestLoader().loadTestsFromTestCase(TestConfigHolderSequence)
     inactive_config_suite = unittest.TestLoader().loadTestsFromTestCase(TestInactiveConfigsSequence)
     file_watcher_event_suite = unittest.TestLoader().loadTestsFromTestCase(TestFileEventHandler)
-    file_watcher_suite = unittest.TestLoader().loadTestsFromTestCase(TestFileWatcherManager)
 
     print "\n\n------ BEGINNING BLOCKSERVER UNIT TESTS ------"
 
@@ -48,6 +46,5 @@ if __name__ == '__main__':
     xmlrunner.XMLTestRunner(output=xml_dir).run(config_holder_suite)
     xmlrunner.XMLTestRunner(output=xml_dir).run(inactive_config_suite)
     xmlrunner.XMLTestRunner(output=xml_dir).run(file_watcher_event_suite)
-    xmlrunner.XMLTestRunner(output=xml_dir).run(file_watcher_suite)
 
     print "------ BLOCKSERVER UNIT TESTS COMPLETE ------\n\n"
