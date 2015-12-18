3195ab06a38bfa395f1298da0ba2ac467dad9294
mattclarke <matt.clarke@stfc.ac.uk>
2015-12-18 12:25:47 +0000

    Did not mean to check these changes in

--- start_blockserver_cmd.bat
+++ start_blockserver_cmd.bat
@@ -3,7 +3,7 @@ REM @echo off
 set MYDIRBLOCK=%~dp0
 call %MYDIRBLOCK%..\..\..\config_env_base.bat
 
-REM %HIDEWINDOW% h
+%HIDEWINDOW% h
 
 set EPICS_CAS_INTF_ADDR_LIST=127.0.0.1
 set EPICS_CAS_BEACON_ADDR_LIST=127.255.255.255
@@ -19,4 +19,4 @@ if exist "%ICPSETTINGSDIR%/gwblock.pvlist" (
 
 set MYDIRVC=%MYDIRBLOCK%..\..\ConfigVersionControl\master
 
-%PYTHON% %MYDIRBLOCK%BlockServer\block_server.py -od %MYDIRBLOCK%..\..\..\iocstartup -sd %MYDIRBLOCK%..\..\..\schema\configurations -cd %ICPCONFIGROOT% -pv %GWBLOCK_PVLIST% -f ISIS
+%PYTHONW% %MYDIRBLOCK%BlockServer\block_server.py -od %MYDIRBLOCK%..\..\..\iocstartup -sd %MYDIRBLOCK%..\..\..\schema\configurations -cd %ICPCONFIGROOT% -pv %GWBLOCK_PVLIST% -f ISIS
