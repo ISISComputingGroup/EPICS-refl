0e6de85c404cd7c19585161c1424e0cabdf5ad35
Dominic Oram <dominic.oram@stfc.ac.uk>
2016-01-07 11:32:20 +0000

    BlockServer sends regex error message

--- BlockServer/core/isis_specific.py
+++ BlockServer/core/isis_specific.py
@@ -2,7 +2,8 @@ from server_common.utilities import compress_and_hex
 import json
 
 DISALLOWED_BLOCK_NAMES = ["lowlimit", "highlimit", "runcontrol", "wait"]
-ALLOWED_BLOCK_REGEX = r"^[a-zA-Z]\w*$"
+ALLOWED_BLOCK_NAME_REGEX = r"^[a-zA-Z]\w*$"
+BLOCK_REGEX_ERROR_MESSAGE = "Block name must start with a letter and only contain letters, numbers and underscores"
 BLOCK_RULES_PV = "BLOCK_RULES"
 
 class BlockRules(object):
@@ -18,5 +19,6 @@ class BlockRules(object):
         self._create_pv()
 
     def _create_pv(self):
-        data = {"disallowed": DISALLOWED_BLOCK_NAMES, "regex": ALLOWED_BLOCK_REGEX}
+        data = {"disallowed": DISALLOWED_BLOCK_NAMES,
+                "regex": ALLOWED_BLOCK_NAME_REGEX, "regex_message": BLOCK_REGEX_ERROR_MESSAGE}
         self._cas.updatePV(BLOCK_RULES_PV, compress_and_hex(json.dumps(data)))
