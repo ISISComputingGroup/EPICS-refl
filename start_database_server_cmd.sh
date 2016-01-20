b99f90fc131c1409e57e7dc84f56a536f97876e8
Michael Hart <michael.hart@stfc.ac.uk>
2016-01-20 14:59:31 +0000

    Added bash scripts for linux startup and shutdown

---
+++ start_database_server_cmd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f ${BASH_SOURCE[0]})
+SCRIPTPATH=`dirname "$SCRIPT"`
+export MYDIRBLOCK=$SCRIPTPATH
+
+# Ensure environment is set up
+if [ -z "$EPICS_ROOT" ]; then
+    . $MYDIRBLOCK/../../../config_env_base.sh
+fi
+
+export EPICS_CAS_INTF_ADDR_LIST="127.0.0.1"
+export EPICS_CAS_BEACON_ADDR_LIST="127.255.255.255"
+
+export PYTHONUNBUFFERED="TRUE"
+
+python "$MYDIRBLOCK/DatabaseServer/database_server.py" -od "$MYDIRBLOCK/../../../iocstartup" -f ISIS
+
