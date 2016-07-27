4c0f75726ebeb53d361e4cc39dfb097a0ba99b79
Sean Marshall <sean-marshall@hotmail.co.uk>
2016-07-27 10:31:57 +0100

    A simple web server that uses BaseHTTPServer

--- WebServer/simple_webserver.py
+++ WebServer/simple_webserver.py
@@ -1,19 +1,15 @@
-import socket
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 HOST, PORT = '', 8888
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listen_socket.bind((HOST, PORT))
-listen_socket.listen(1)
+
+class myHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        self.wfile.write("Hello World!")
+
+
+server=HTTPServer(('',PORT), myHandler)
 print "Serving HTTP on port %s ..." % PORT
-while True:
-    client_connection, client_address = listen_socket.accept()
-    request = client_connection.recv(1024)
-    print request
-
-    http_response = """\
-HTTP/1.1 200 OK
-
-<b>Hello World!</b>
-"""
-    client_connection.sendall(http_response)
-    client_connection.close()
+server.serve_forever()
