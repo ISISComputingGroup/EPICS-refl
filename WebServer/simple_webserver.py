432040cd48740c7eb79ab412b43fa7b3580c5ca4
Dominic Oram <dominic.oram@stfc.ac.uk>
2016-07-29 10:29:04 +0100

    Fixed passing the text around

--- WebServer/simple_webserver.py
+++ WebServer/simple_webserver.py
@@ -1,16 +1,19 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from threading import Thread
 HOST, PORT = '', 8008
+from time import sleep
 
-__text = ""
+# This is a global variable as it's easier with threading and the way the handler class is defined
+# TODO: Fix later!!
+_response = ""
 
 class myHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        global __text
-        self.wfile.write(__text)
+        global _response
+        self.wfile.write(_response)
 
 class Server(Thread):
 
@@ -20,11 +23,13 @@ class Server(Thread):
         self.server.serve_forever()
 
     def set_text(self,set_text_to):
-        global __text
-        __text = set_text_to
+        global _response
+        _response = set_text_to
 
 
 if __name__ == '__main__':
     server = Server()
     server.start()
     server.set_text("TEST")
+    sleep(10)
+    server.set_text("NOT TEST")
