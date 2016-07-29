00f835ac13f3326d334fdfd3d27d58a69e468a5f
Sean Marshall <sean-marshall@hotmail.co.uk>
2016-07-29 10:22:49 +0100

    Trying to pass text to handler

--- WebServer/simple_webserver.py
+++ WebServer/simple_webserver.py
@@ -2,29 +2,29 @@ from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from threading import Thread
 HOST, PORT = '', 8008
 
+__text = ""
+
 class myHandler(BaseHTTPRequestHandler):
-    text = ""
-
-    def set_text(self):
-        global text
-        text = server.set_text
-
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        self.wfile.write(self.text)
+        global __text
+        self.wfile.write(__text)
 
 class Server(Thread):
-    def run(self):
-        server=HTTPServer(('',PORT), myHandler)
-        print "Serving HTTP on port %s ..." % PORT
-        server.serve_forever()
 
-    def set_text(self):
-        global text
-        text = server.set_text
+    def run(self):
+        self.server = HTTPServer(('',PORT), myHandler)
+        print "Serving HTTP on port %s ..." % PORT
+        self.server.serve_forever()
+
+    def set_text(self,set_text_to):
+        global __text
+        __text = set_text_to
+
 
 if __name__ == '__main__':
     server = Server()
     server.start()
+    server.set_text("TEST")
