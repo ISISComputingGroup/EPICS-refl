ad68b30767daecdefe2f176c229df73d16db5b25
simonfernandes <simon.fernandes@stfc.ac.uk>
2016-08-02 14:44:45 +0100

    add test and tweak comments

--- server_common/test_modules/utilities_tests.py
+++ server_common/test_modules/utilities_tests.py
@@ -10,52 +10,85 @@ class TestCreatePVName(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_when_pv_name_contains_whitespace_then_replace_with_underscore(self):
-        pv = create_pv_name("config name", [], "peevee")
+    def test_WHEN_pv_name_contains_whitespace_THEN_replace_whitespace_with_underscore(self):
+        # Act
+        pv = create_pv_name("config name", [], "PEEVEE")
 
+        # Assert
         self.assertEquals(pv, "CONFIG")
 
-    def test_when_pv_name_contains_non_alphanumeric_characters_then_remove(self):
-        pv = create_pv_name("c-onf@ig", [], "peevee")
+    def test_WHEN_pv_name_contains_non_alphanumeric_characters_THEN_remove_non_alphanumeric_characters(self):
+        # Act
+        pv = create_pv_name("c-onf@ig", [], "PEEVEE")
 
+        # Assert
         self.assertEquals(pv, "CONFIG")
 
-    def test_when_pv_name_contains_only_numbers_and_underscores_then_replace_with_default_pv_name(self):
-        pv = create_pv_name("1_2_3_4", [], "peevee")
+    def test_WHEN_pv_name_contains_only_numbers_and_underscores_THEN_replace_pv_with_default_pv_name(self):
+        # Act
+        pv = create_pv_name("1_2_3_4", [], "PEEVEE")
 
-        self.assertEquals(pv, "peevee")
+        # Assert
+        self.assertEquals(pv, "PEEVEE")
 
-    def test_when_pv_name_is_blank_then_replace_with_default_pv_name(self):
-        pv = create_pv_name("", [], "peevee")
+    def test_WHEN_pv_name_is_blank_THEN_replace_pv_with_default_pv_name(self):
+        # Act
+        pv = create_pv_name("", [], "PEEVEE")
 
-        self.assertEquals(pv, "peevee")
+        # Assert
+        self.assertEquals(pv, "PEEVEE")
 
-    def test_given_blank_pv_name_when_another_blank_pv_name_then_append_numbers(self):
-        pv_01 = create_pv_name("", [], "peevee")
-        pv_02 = create_pv_name("", [pv_01], "peevee")
-        pv_03 = create_pv_name("", [pv_01, pv_02], "peevee")
+    def test_GIVEN_blank_pv_name_WHEN_another_blank_pv_name_THEN_append_numbers(self):
+        # Arrange
+        pv_01 = create_pv_name("", [], "PEEVEE")
 
-        self.assertEquals(pv_01, "peevee")
+        # Act
+        pv_02 = create_pv_name("", [pv_01], "PEEVEE")
+        pv_03 = create_pv_name("", [pv_01, pv_02], "PEEVEE")
+
+        # Assert
+        self.assertEquals(pv_01, "PEEVEE")
         self.assertEquals(pv_02, "peev01")
         self.assertEquals(pv_03, "peev02")
 
-    def test_when_pv_name_too_long_then_truncate(self):
-        pv = create_pv_name("Configuration", [], "peevee")
+    def test_WHEN_pv_name_too_long_THEN_truncate_to_six_chars(self):
+        # Act
+        pv = create_pv_name("Configuration", [], "PEEVEE")
 
+        # Assert
         self.assertEquals(pv, "CONFIG")
 
-    def test_given_an_existing_pv_when_another_pv_of_same_name_then_rename_differently(self):
-        pv_01 = create_pv_name("Conf", [], "peevee")
-        pv_02 = create_pv_name("Conf", [pv_01], "peevee")
+    def test_GIVEN_an_existing_pv_WHEN_create_another_pv_of_same_name_THEN_append_numbers(self):
+        # Arrange
+        pv_01 = create_pv_name("Conf", [], "PEEVEE")
 
+        # Act
+        pv_02 = create_pv_name("Conf", [pv_01], "PEEVEE")
+
+        # Assert
         self.assertEquals(pv_01, "CONF")
         self.assertEquals(pv_02, "CONF01")
 
-    def test_given_an_existing_truncated_pv_when_another_pv_of_same_name_then_truncate_and_rename_differently(self):
-        pv_01 = create_pv_name("Configuration", [], "peevee")
-        pv_02 = create_pv_name("Configuration", [pv_01], "peevee")
-        pv_03 = create_pv_name("Configuration", [pv_01, pv_02], "peevee")
+    def test_GIVEN_an_existing_truncated_pv_WHEN_another_pv_of_same_name_THEN_truncate_and_rename_differently(self):
+        # Arrange
+        pv_01 = create_pv_name("Configuration", [], "PEEVEE")
 
+        # Act
+        pv_02 = create_pv_name("Configuration", [pv_01], "PEEVEE")
+        pv_03 = create_pv_name("Configuration", [pv_01, pv_02], "PEEVEE")
+
+        # Assert
         self.assertEquals(pv_01, "CONFIG")
         self.assertEquals(pv_02, "CONF01")
         self.assertEquals(pv_03, "CONF02")
+
+    def test_GIVEN_a_long_pv_WHEN_create_pv_with_same_name_and_invalid_chars_THEN_remove_invalid_chars_and_truncate_and_number_correctly(self):
+        # Arrange
+        pv_01 = create_pv_name("Configuration", [], "PEEVEE")
+
+        # Act
+        pv_02 = create_pv_name("Co*&^nfiguration", [pv_01], "PEEVEE")
+
+        # Assert
+        self.assertEquals(pv_01, "CONFIG")
+        self.assertEquals(pv_02, "CONF01")
