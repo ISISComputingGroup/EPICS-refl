f0dea130613255f42cfba848c4c5478e6fdc2fb6
AdrianPotter <adrian.potter@tessella.com>
2016-09-08 16:09:24 +0100

    Remove tests for removed PV

--- DatabaseServer/test_modules/database_server_test_mysql.py
+++ DatabaseServer/test_modules/database_server_test_mysql.py
@@ -30,18 +30,6 @@ class TestDatabaseServer(unittest.TestCase):
         self.db_server = DatabaseServer(self.ms, TEST_DB, os.path.abspath('./test_files'), True)
         self.db_server._update_individual_interesting_pvs()
 
-    def test_interest_high_pvs_exist(self):
-        on_fly_pvs = self.ms.pv_list
-        self.assertTrue("INTERESTING_PVS:TESTIOC:HIGH" in on_fly_pvs.keys())
-        self.assertTrue("INTERESTING_PVS:SIMPLE1:HIGH" in on_fly_pvs.keys())
-        self.assertTrue("INTERESTING_PVS:SIMPLE2:HIGH" in on_fly_pvs.keys())
-
-    def test_interest_medium_pvs_exist(self):
-        on_fly_pvs = self.ms.pv_list
-        self.assertTrue("INTERESTING_PVS:TESTIOC:MEDIUM" in on_fly_pvs.keys())
-        self.assertTrue("INTERESTING_PVS:SIMPLE1:MEDIUM" in on_fly_pvs.keys())
-        self.assertTrue("INTERESTING_PVS:SIMPLE2:MEDIUM" in on_fly_pvs.keys())
-
     def test_interest_high_pvs_correct(self):
         on_fly_pvs = self.ms.pv_list
         data = [json.loads(dehex_and_decompress(x)) for x in on_fly_pvs.values()]
