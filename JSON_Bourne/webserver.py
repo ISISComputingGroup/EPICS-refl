588cea2a14867d7a9cc65e724994995bb4a5e217
Matt Clarke <matt.clarke@stfc.ac.uk>
2016-09-30 10:48:30 +0100

    Update webserver.py

--- JSON_Bourne/webserver.py
+++ JSON_Bourne/webserver.py
@@ -13,12 +13,18 @@ class MyHandler(BaseHTTPRequestHandler):
         This is called by BaseHTTPRequestHandler every time a client does a GET.
         The response is written to self.wfile
         """
-        if self.path == '/favicon.ico':
-            pass
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(json.dumps(scrape_webpage()))
+        
+        # Look for the callback
+        # JSONP requires a response of the format "name_of_callback(json_string)"
+        # e.g. myFunction({ "a": 1, "b": 2})
+        result = re.search('/?callback=(\w+)&_', self.path)
+        if len(result.groups()) > 0:
+            callback = result.groups()[0]
+            ans = "%s(%s)" % (callback, json.dumps(scrape_webpage()))
+            self.wfile.write(ans)
 
     def log_message(self, format, *args):
         """ By overriding this method and doing nothing we disable writing to console
