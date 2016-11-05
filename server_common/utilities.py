711562fcfdda8eee29f74430766df08af729cc38
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2016-11-05 17:38:20 +0000

    Add comment on utf-8 usage

--- server_common/utilities.py
+++ server_common/utilities.py
@@ -81,7 +81,8 @@ def convert_to_json(value):
     Returns:
         string : The JSON representation of the inputted object
     """
-#    return json.dumps(value).encode('utf-8', 'replace')
+# TODO: we may want to use 'utf-8' here in future, not needed 
+#       this time as functionality previously duplicated in exp_data.py
     return json.dumps(value).encode('ascii', 'replace')
 
 
