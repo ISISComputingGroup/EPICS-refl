b2184424f5a9f6dc3ea41bb0fe1fa16b52848ba7
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-03-31 11:03:34 +0100

    Update alarm server

--- BlockServer/alarm/load_alarm_config.py
+++ BlockServer/alarm/load_alarm_config.py
@@ -6,39 +6,49 @@ import time
 
 class AlarmConfigLoader(object):
 
+    # Instance of this singleton
     instance = None
-    loading = False
+    # The subprocess that the alarm server reloads itself in
+    process = None
 
     @staticmethod
     def get_instance():
-        if AlarmConfigLoader.instance is not None:
-            return AlarmConfigLoader.instance
-        else:
+
+        if AlarmConfigLoader.instance is None:
             AlarmConfigLoader.instance = AlarmConfigLoader()
-            return AlarmConfigLoader.instance
+
+        return AlarmConfigLoader.instance
 
     def _load(self):
         """
         Simple wrapper around a batch script which reloads the configuration in the alarm server.
         """
-        AlarmConfigLoader.loading = True
 
-        print "Alarm server sleeping"
+        # Terminate the last batch job if it is still running
+        # (prevents two instances from running concurrently which might cause problems).
+        if self.process is not None:
+            self.process.kill()
+
+        print "Alarm server will update in 20 seconds\n"
         time.sleep(20)
-        print "Alarm server finished sleeping"
-        filepath = "C:\Instrument\Apps\EPICS\CSS\master\AlarmServer\load_alarm_config.bat"
-        with open(os.devnull, 'w') as devnull:
-            p = subprocess.Popen(filepath, shell=True, stdout=devnull, stderr=devnull)
+        print "Alarm server updating...\n"
 
-        p.wait()
-        AlarmConfigLoader.loading = False
-        print "Alarm server updated"
+        filepath = os.path.join('C:\\', 'Instrument', 'Apps', 'EPICS', 'CSS', 'master', 'AlarmServer', 'run_alarm_server.bat')
+
+        try:
+            with open(filepath, 'r') and open(os.devnull) as devnull:
+                self.process = subprocess.Popen(filepath, stderr=devnull)
+                self.process.communicate(input=None)
+        except Exception as e:
+            print "Error while reloading alarm server."
+            print e
+
+        print "Alarm server updated\n"
 
     def load_in_new_thread(self):
-        if not AlarmConfigLoader.loading:
-            thread = threading.Thread(target=self._load)
-            thread.start()
+        thread = threading.Thread(target=self._load)
+        thread.start()
 
 if __name__ == "__main__":
     AlarmConfigLoader.get_instance().load_in_new_thread()
-    print "finished"
+    print "finished\n"
