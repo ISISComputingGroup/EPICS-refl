ad1a115e6f07832405498bd6f1d561e1438b9166
rerpha <jack.harper@stfc.ac.uk>
2017-10-10 10:55:08 +0100

    function call was redundant, used other api function to check if topic is in list of topics on broker

--- BlockServerToKafka/kafka_producer.py
+++ BlockServerToKafka/kafka_producer.py
@@ -25,13 +25,15 @@ class Producer:
 
     def __init__(self, server, config_topic, data_topic):
         self.topic = config_topic
-        self.producer, self.client = self.set_up_producer(server)
         self.converter = ForwarderConfig(data_topic)
+        self.set_up_producer(server)
 
     def set_up_producer(self, server):
         try:
-            client = SimpleClient(server)
-            producer = KafkaProducer(bootstrap_servers=server)
+            self.client = SimpleClient(server)
+            self.producer = KafkaProducer(bootstrap_servers=server)
+            if not self.topic_exists(self.topic):
+                print_and_log("WARNING: topic {} does not exist. It will be created by default.".format(self.topic))
         except errors.NoBrokersAvailable:
             print_and_log("No brokers found on server: " + server[0])
             quit()
@@ -44,7 +46,7 @@ class Producer:
         except errors.InvalidTopicError:
             print_and_log("Invalid topic, to enable auto creation of topics set"
                           " auto.create.topics.enable to false in broker configuration")
-        return producer, client
+
 
     def add_config(self, pvs):
         """
@@ -56,14 +58,13 @@ class Producer:
         Returns:
             None.
         """
-        if not self.topic_exists(self.topic):
-            print_and_log("WARNING: topic {} does not exist. It will be created by default.".format(self.topic))
+
         data = self.converter.create_forwarder_configuration(pvs)
         print_and_log("Sending data {}".format(data))
         self.producer.send(self.topic, bytes(data))
 
     def topic_exists(self, topicname):
-        return self.client.ensure_topic_exists(topicname)
+        return topicname in self.client.topics
 
     def remove_config(self, pvs):
         """
