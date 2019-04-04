01dc6f586d229f494107204d9fef78783690ca6f
Tom Willemsen <tomwillemsen1995@gmail.com>
2019-04-04 16:57:46 +0100

    Replace mutable defaults with None and set them in constructor.
    Although not necessarily causing a problem here, mutable default arguments are not usually a good idea in Python.

--- BlockServer/mocks/mock_ioc.py
+++ BlockServer/mocks/mock_ioc.py
@@ -14,20 +14,26 @@
 # https://www.eclipse.org/org/documents/epl-v10.php or
 # http://opensource.org/licenses/eclipse-1.0.php
 
-class MockIoc(object):
 
-    def __init__(self, name="NEWIOC", autostart=True, restart=True, macros=[], pvs=[], pvsets=[], component=None,
+class MockIoc(object):
+    def __init__(self, name="NEWIOC", autostart=True, restart=True, macros=None, pvs=None, pvsets=None, component=None,
                  simlevel='none'):
         self.name = name
         self.autostart = autostart
         self.restart = restart
+        if macros is None:
+            macros = []
         self.macros = macros
+        if pvs is None:
+            pvs = []
         self.pvs = pvs
+        if pvsets is None:
+            pvsets = []
         self.pvsets = pvsets
         self.component = component
         self.simlevel = simlevel
 
-    def get(self,name):
+    def get(self, name):
         return self.__getattribute__(name)
 
     def __getitem__(self, name):
