c679c425424294fe9e9904da77b978850485b504
Tom Willemsen <tomwillemsen1995@gmail.com>
2019-06-25 13:48:15 +0100

    Correct format for config files

--- RemoteIocServer/config_monitor.py
+++ RemoteIocServer/config_monitor.py
@@ -6,7 +6,27 @@ import os
 
 from genie_python.genie_cachannel_wrapper import CaChannelWrapper
 
-REMOTE_IOC_CONFIG_NAME = "_REMOTE_IOC_CONFIG"
+REMOTE_IOC_CONFIG_NAME = "_REMOTE_IOC"
+
+EMPTY_COMPONENTS_XML = """<?xml version="1.0" ?>
+<components xmlns="http://epics.isis.rl.ac.uk/schema/components/1.0" xmlns:comp="http://epics.isis.rl.ac.uk/schema/components/1.0" xmlns:xi="http://www.w3.org/2001/XInclude"/>
+"""
+
+EMPTY_GROUPS_XML = """<?xml version="1.0" ?>
+<groups xmlns="http://epics.isis.rl.ac.uk/schema/groups/1.0" xmlns:grp="http://epics.isis.rl.ac.uk/schema/groups/1.0" xmlns:xi="http://www.w3.org/2001/XInclude"/>
+"""
+
+EMPTY_BLOCKS_XML = """<?xml version="1.0" ?>
+<blocks xmlns="http://epics.isis.rl.ac.uk/schema/blocks/1.0" xmlns:blk="http://epics.isis.rl.ac.uk/schema/blocks/1.0" xmlns:xi="http://www.w3.org/2001/XInclude"></blocks>
+"""
+
+META_XML = """<?xml version="1.0" ?>
+<meta>
+	<description>Configuration for remote IOC</description>
+	<synoptic>-- NONE --</synoptic>
+	<edits></edits>
+</meta>
+"""
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 from server_common.channel_access import ChannelAccess
@@ -53,7 +73,7 @@ class ConfigurationMonitor(object):
         self._monitor = _ConfigurationMonitor("{}CS:BLOCKSERVER:GET_CURR_CONFIG_DETAILS".format(self._remote_pv_prefix))
         self._monitor.start(callback=self._config_updated)
 
-    def _config_updated(self, value, alarm_severity_ignored, alarm_status_ignored):
+    def _config_updated(self, value, *_, **__):
         try:
             new_config = dehex_and_decompress(waveform_to_string(value))
             write_new_config_as_xml(new_config)
@@ -85,7 +105,7 @@ def write_new_config_as_xml(config_json):
             autostart=ioc["autostart"],
             restart=ioc["restart"],
             component=None,  # We don't care what component it was defined in.
-            macros={macro["name"]: {macro["name"]: macro["value"]} for macro in ioc["macros"]},  # This format is what the blockserver wants - despite it being weird
+            macros={macro["name"]: {"name": macro["name"], "value": macro["value"]} for macro in ioc["macros"]},
             pvsets={pvset["name"]: {"name": pvset["name"], "value": pvset["value"]} for pvset in ioc["pvsets"]},
             pvs={pv["name"]: {"name": pv["name"], "value": pv["value"]} for pv in ioc["pvs"]},
             simlevel=ioc["simlevel"]
@@ -93,17 +113,35 @@ def write_new_config_as_xml(config_json):
 
     iocs_xml = ConfigurationXmlConverter.iocs_to_xml(iocs)
 
-    print_and_log("Writing new iocs.xml file")
+    print_and_log("Writing new config files")
 
     config_base = os.path.normpath(os.getenv("ICPCONFIGROOT"))
     config_dir = os.path.join(config_base, "configurations", REMOTE_IOC_CONFIG_NAME)
     if not os.path.exists(config_dir):
         os.mkdir(config_dir)
 
+    print_and_log("Writing iocs.xml")
     with open(os.path.join(config_dir, "iocs.xml"), "w") as f:
         f.write(str(iocs_xml))
 
-    with open(os.path.join(config_base, "last_config.txt"), "w") as f:
-        f.write(REMOTE_IOC_CONFIG_NAME)
+    print_and_log("Writing components.xml")
+    with open(os.path.join(config_dir, "components.xml"), "w") as f:
+        f.write(EMPTY_COMPONENTS_XML)
 
-    print_and_log("Finished writing new iocs.xml file")
+    print_and_log("Writing blocks.xml")
+    with open(os.path.join(config_dir, "blocks.xml"), "w") as f:
+        f.write(EMPTY_BLOCKS_XML)
+
+    print_and_log("Writing groups.xml")
+    with open(os.path.join(config_dir, "groups.xml"), "w") as f:
+        f.write(EMPTY_GROUPS_XML)
+
+    print_and_log("Writing meta.xml")
+    with open(os.path.join(config_dir, "meta.xml"), "w") as f:
+        f.write(META_XML)
+
+    print_and_log("Writing last_config.txt")
+    with open(os.path.join(config_base, "last_config.txt"), "w") as f:
+        f.write("{}\n".format(REMOTE_IOC_CONFIG_NAME))
+
+    print_and_log("Finished writing new config")
