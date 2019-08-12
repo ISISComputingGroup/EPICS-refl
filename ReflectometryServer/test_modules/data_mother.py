a2e3bb803aa84348a219626bf997eecb0648bd64
ThomasLohnert <thomas.lohnert@stfc.ac.uk>
2019-08-12 16:37:53 +0100

    Add beamline init tests for non-zero natural beam angle.

--- ReflectometryServer/test_modules/data_mother.py
+++ ReflectometryServer/test_modules/data_mother.py
@@ -1,7 +1,7 @@
 """
 Test data and classes.
 """
-from math import tan, radians
+from math import tan, radians, sin, cos
 
 from utils import DEFAULT_TEST_TOLERANCE
 
@@ -107,21 +107,29 @@ class DataMother(object):
         return bl, axes
 
     @staticmethod
-    def beamline_sm_theta_detector(sm_angle, theta, det_offset=0, autosave_theta_not_offset=True):
+    def beamline_sm_theta_detector(sm_angle, theta, det_offset=0, autosave_theta_not_offset=True, beam_angle=0.0):
         """
-        Create beamline with Slits 1 and 3 a theta and a detector
+        Create beamline with supermirror, theta and a tilting detector.
+
         Args:
-            spacing: spacing between components
+            sm_angle (float): The initialisation value for supermirror angle
+            theta (float): The initialisation value for theta
+            det_offset (float): The initialisation value for detector offset
+            autosave_theta_not_offset (bool):
+            beam_start (PositionAndAngle):
 
         Returns: beamline, axes
 
         """
+        beam_start = PositionAndAngle(0.0, 0.0, 0.0)
+        perp_to_floor_angle_in_mantid = 90 + beam_angle
+
         # COMPONENTS
         z_sm_to_sample = 1
         z_sample_to_det = 2
-        sm_comp = ReflectingComponent("sm_comp", PositionAndAngle(0.0, 0, 90))
-        detector_comp = TiltingComponent("detector_comp", PositionAndAngle(0.0, z_sm_to_sample + z_sample_to_det, 90))
-        theta_comp = ThetaComponent("theta_comp", PositionAndAngle(0.0, z_sm_to_sample, 90), [detector_comp])
+        sm_comp = ReflectingComponent("sm_comp", PositionAndAngle(0.0, 0, perp_to_floor_angle_in_mantid))
+        detector_comp = TiltingComponent("detector_comp", PositionAndAngle(0.0, z_sm_to_sample + z_sample_to_det, perp_to_floor_angle_in_mantid))
+        theta_comp = ThetaComponent("theta_comp", PositionAndAngle(0.0, z_sm_to_sample, perp_to_floor_angle_in_mantid), [detector_comp])
 
         comps = [sm_comp, theta_comp, detector_comp]
 
@@ -135,11 +143,12 @@ class DataMother(object):
 
         # DRIVERS
         beam_angle_after_sample = theta * 2 + sm_angle * 2
-        offset_from_sm_angle = z_sm_to_sample * tan(radians(sm_angle * 2))
-        offset_from_theta = z_sample_to_det * tan(radians(beam_angle_after_sample))
+        supermirror_segment = (z_sm_to_sample, sm_angle)
+        theta_segment = (z_sample_to_det, theta)
+        reflection_offset = DataMother._calc_reflection_offset(beam_angle, [supermirror_segment, theta_segment])
         sm_axis = create_mock_axis("MOT:MTR0101", sm_angle, 1)
-        det_axis = create_mock_axis("MOT:MTR0104", offset_from_sm_angle + offset_from_theta + det_offset, 1)
-        det_angle_axis = create_mock_axis("MOT:MTR0105", beam_angle_after_sample, 1)
+        det_axis = create_mock_axis("MOT:MTR0104", reflection_offset + det_offset, 1)
+        det_angle_axis = create_mock_axis("MOT:MTR0105",  beam_start.angle + beam_angle_after_sample, 1)
         axes = {"sm_axis": sm_axis,
                 "det_axis": det_axis,
                 "det_angle_axis": det_angle_axis}
@@ -152,10 +161,37 @@ class DataMother(object):
         nr_inits = {}
         nr_mode = BeamlineMode("NR", [param.name for param in params], nr_inits)
         modes = [nr_mode]
-        beam_start = PositionAndAngle(0.0, 0.0, -1)
         bl = Beamline(comps, params, drives, modes, beam_start)
         bl.active_mode = nr_mode.name
         return bl, axes
+
+    @staticmethod
+    def _calc_reflection_offset(beam_angle, segments):
+        """
+        Calculates a position offset to the beam intercept for a linear axis given an ordered list of beam segments,
+        each of which adds an angle to the beam.
+
+        Params:
+            beam_angle (float): The angle of the straight through beam to the linear motion axes
+            segments (tuple[float, float]): A list of beam segments made of tuples with (segment_length, added angle)
+
+        Returns: The total offset for the linear axis
+        """
+        total_offset = 0.0
+
+        reflection_angles = []
+        for distance, added_angle in segments:
+            reflection_angles.append(added_angle)
+
+            cumulative_reflection_angle = 0
+            for reflection_angle in reflection_angles:
+                cumulative_reflection_angle += 2*reflection_angle
+
+            offset_1 = distance * sin(radians(beam_angle))
+            offset_2 = distance * cos(radians(beam_angle)) * tan(radians(cumulative_reflection_angle - beam_angle))
+            total_offset += offset_1 + offset_2
+
+        return total_offset
 
 
 def create_mock_axis(name, init_position, max_velocity, backlash_distance=0, backlash_velocity=1, direction="Pos"):
