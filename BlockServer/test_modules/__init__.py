ae23067fa30e62142b574ae1d9fa5990b144df93
JamesKingWork <JamesKingWork@ndlt1208.isis.cclrc.ac.uk>
2019-12-05 15:36:38 +0000

    Skip linux build

--- BlockServer/test_modules/__init__.py
+++ BlockServer/test_modules/__init__.py
@@ -16,6 +16,7 @@
 import os
 import six
 import unittest
+from server_common.constants import IS_LINUX
 
 
 def load_tests(loader, standard_tests, pattern):
@@ -25,7 +26,7 @@ def load_tests(loader, standard_tests, pattern):
 
     The tests in this module are only added under Python 2.
     """
-    if six.PY2:
+    if six.PY2 and not IS_LINUX:
         standard_tests.addTests(loader.discover(os.path.dirname(__file__), pattern=pattern))
         return standard_tests
     else:
