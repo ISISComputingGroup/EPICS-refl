9cadef656f341fd9bfbc8127c26a9b526dda7935
ThomasLohnert <thomas.lohnert@stfc.ac.uk>
2020-03-31 12:47:32 +0100

    Add tests on tolerance

--- ReflectometryServer/test_modules/test_out_of_beam_lookup.py
+++ ReflectometryServer/test_modules/test_out_of_beam_lookup.py
@@ -29,7 +29,6 @@ class TestComponentWithOutOfBeamPositions(unittest.TestCase):
 
         assert_that(actual.position, is_(expected))
 
-
     @parameterized.expand([(-5, PARK_LOW_POS, True),
                            (-5, PARK_HIGH_POS, False),
                            (-5, PARK_HIHI_POS, True),
@@ -49,5 +48,43 @@ class TestComponentWithOutOfBeamPositions(unittest.TestCase):
 
         assert_that(actual, is_(expected))
 
-    def test_GIVEN_no_out_of_beam_position_WHEN_checking_whether_component_is_in_beam_THEN_returns_true(self):
+    # test for a range of values
+    @parameterized.expand([(-5, PARK_LOW_POS),
+                           (-5, PARK_HIGH_POS),
+                           (-5, PARK_HIHI_POS),
+                           (0, PARK_LOW_POS),
+                           (0, PARK_HIGH_POS),
+                           (0, PARK_HIHI_POS),
+                           (20, PARK_LOW_POS),
+                           (20, PARK_HIGH_POS),
+                           (20, PARK_HIHI_POS),
+                           ])
+    def test_GIVEN_no_out_of_beam_position_WHEN_checking_whether_component_is_in_beam_THEN_returns_true(self, beam_height, displacement):
+        expected = True
         lookup = OutOfBeamLookup([])
+        beam_intercept = Position(beam_height, 0)
+
+        actual = lookup.is_in_beam(beam_height, displacement)
+
+        assert_that(actual, is_(expected))
+
+    @parameterized.expand([(PARK_LOW_POS, 0.5, False),
+                           (PARK_LOW_POS, 2, True),
+                           (PARK_LOW_POS, 4, True),
+                           (PARK_HIGH_POS, 0.5, False),
+                           (PARK_HIGH_POS, 2, False),
+                           (PARK_HIGH_POS, 4, True),])
+    def test_GIVEN_positions_with_tolerance_WHEN_checking_whether_component_is_in_beam_THEN_returns_correct_status_for_each_position(self, position_to_check, offset_from_pos, expected):
+        beam_intercept = Position(0, 0)
+        if position_to_check is PARK_HIGH_POS:
+            beam_intercept = Position(2, 0)
+        tolerance_low = 0.5
+        tolerance_high = 2
+        park_low_with_tolerance = OutOfBeamPosition(PARK_LOW_POS, tolerance=tolerance_low)
+        park_high_with_tolerance = OutOfBeamPosition(PARK_HIGH_POS, threshold=1, tolerance=tolerance_high)
+
+        lookup = OutOfBeamLookup([park_low_with_tolerance, park_high_with_tolerance])
+
+        for position in [position_to_check + offset_from_pos, position_to_check - offset_from_pos]:
+            in_beam_status = lookup.is_in_beam(beam_intercept, position)
+            assert_that(in_beam_status, is_(expected))
