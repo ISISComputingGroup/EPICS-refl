de9b82f5a31d25e3ee052a9f0e8b1e952b756a2d
John-Holt-Tessella <John.Holt@tessella.com>
2020-06-17 13:57:28 +0100

    Update comment

--- ReflectometryServer/test_modules/__init__.py
+++ ReflectometryServer/test_modules/__init__.py
@@ -8,7 +8,7 @@ def load_tests(loader, standard_tests, pattern):
     This function is needed by the load_tests protocol described at
     https://docs.python.org/3/library/unittest.html#load-tests-protocol
 
-    The tests in this module are only added under Python 2.
+    The tests in this module are only added under Python 3.
     """
     if six.PY2:
         return unittest.TestSuite()
