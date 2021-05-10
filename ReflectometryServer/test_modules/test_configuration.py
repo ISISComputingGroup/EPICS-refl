a0cb4a3d84936f9cb8741cc202b2a4adb606392d
Dominic Oram <dominic.oram@stfc.ac.uk>
2021-05-10 14:44:46 +0100

    Change macros in tests to be strings rather than booleans

--- ReflectometryServer/test_modules/test_configuration.py
+++ ReflectometryServer/test_modules/test_configuration.py
@@ -9,6 +9,7 @@ import ReflectometryServer.server_status_manager
 from ReflectometryServer.test_modules.test_config.refl.config import OPTIONAL_PARAM_1
 from ReflectometryServer.test_modules.test_config.refl.other_config import OTHER_CONFIG_PARAM
 
+PATH_TO_CONFGIS = os.path.abspath(os.path.join(os.path.dirname(__file__), "test_config", "refl"))
 
 class TestConfiguration(unittest.TestCase):
 
@@ -17,13 +18,13 @@ class TestConfiguration(unittest.TestCase):
         ConfigHelper.reset()
 
     def test_WHEN_loading_valid_beamline_configuration_file_THEN_correct_PVs_are_created(self):
-        beamline_configuration.REFL_CONFIG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "test_config", "refl"))
+        beamline_configuration.REFL_CONFIG_PATH = PATH_TO_CONFGIS
         beamline = beamline_configuration.create_beamline_from_configuration({})
         #  Check Status PV
         self.assertEqual(STATUS.OKAY, ReflectometryServer.server_status_manager.STATUS_MANAGER.status)
 
     def test_WHEN_loading_invalid_beamline_configuration_file_THEN_status_PV_shows_error(self):
-        beamline_configuration.REFL_CONFIG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "test_config", "refl"))
+        beamline_configuration.REFL_CONFIG_PATH = PATH_TO_CONFGIS
         beamline = beamline_configuration.create_beamline_from_configuration({"CONFIG_FILE": "invalid_config.py"})
         #  Check Status PV
         self.assertEqual(STATUS.ERROR, ReflectometryServer.server_status_manager.STATUS_MANAGER.status)
@@ -36,22 +37,22 @@ class TestConfiguration(unittest.TestCase):
         self.assertEqual(STATUS.ERROR, ReflectometryServer.server_status_manager.STATUS_MANAGER.status)
 
     def test_GIVEN_valid_other_config_file_in_macro_WHEN_loading_config_THEN_other_config_is_loaded(self):
-        beamline_configuration.REFL_CONFIG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "test_config", "refl"))
+        beamline_configuration.REFL_CONFIG_PATH = PATH_TO_CONFGIS
         beamline = beamline_configuration.create_beamline_from_configuration({"CONFIG_FILE": "other_config.py"})
         #  Check Status PV
         self.assertEqual(STATUS.OKAY, ReflectometryServer.server_status_manager.STATUS_MANAGER.status)
         self.assertTrue(OTHER_CONFIG_PARAM in beamline.parameters.keys())
 
     def test_GIVEN_optional_macro_set_WHEN_loading_config_THEN_optional_items_part_of_config(self):
-        beamline_configuration.REFL_CONFIG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "test_config", "refl"))
-        beamline = beamline_configuration.create_beamline_from_configuration({"OPTIONAL_1": True})
+        beamline_configuration.REFL_CONFIG_PATH = PATH_TO_CONFGIS
+        beamline = beamline_configuration.create_beamline_from_configuration({"OPTIONAL_1": "True"})
         #  Check Status PV
         self.assertEqual(STATUS.OKAY, ReflectometryServer.server_status_manager.STATUS_MANAGER.status)
         self.assertTrue(OPTIONAL_PARAM_1 in beamline.parameters.keys())
 
     def test_GIVEN_optional_macro_not_set_WHEN_loading_config_THEN_optional_items_not_part_of_config(self):
-        beamline_configuration.REFL_CONFIG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "test_config", "refl"))
-        beamline = beamline_configuration.create_beamline_from_configuration({"OPTIONAL_1": False})
+        beamline_configuration.REFL_CONFIG_PATH = PATH_TO_CONFGIS
+        beamline = beamline_configuration.create_beamline_from_configuration({"OPTIONAL_1": "False"})
         #  Check Status PV
         self.assertEqual(STATUS.OKAY, ReflectometryServer.server_status_manager.STATUS_MANAGER.status)
         self.assertTrue(OPTIONAL_PARAM_1 not in beamline.parameters.keys())
