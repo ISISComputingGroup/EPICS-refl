414e9bf8acdeba51ff9afda321387b37b98e5eea
ThomasLohnert <thomas.lohnert@stfc.ac.uk>
2021-05-17 15:42:23 +0100

    preliminary test

--- ReflectometryServer/test_modules/test_beamline.py
+++ ReflectometryServer/test_modules/test_beamline.py
@@ -241,6 +241,15 @@ class TestRealistic(unittest.TestCase):
                         "call autosaving {}".format(save_name))
 
 
+    def test_GIVEN_negative_sm_angle_WHEN_calculating_bench_intercept_THEN_no_error_thrown(self):
+        expected_theta = 0.2
+        driver_bench_offset = 0
+
+        bl, drives = DataMother.beamline_sm_theta_bench(0.0, expected_theta, driver_bench_offset,
+                                                        natural_angle=2.3)
+        bl.parameter("sm_angle").sp = -0.1
+
+
 class TestBeamlineValidation(unittest.TestCase):
 
     def test_GIVEN_two_beamline_parameters_with_same_name_WHEN_construct_THEN_error(self):
