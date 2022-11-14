c997a86598478599f905c4fb61b4097668197bbf
Nikola Roev <nikola.roev@stfc.ac.uk>
2022-11-14 09:21:36 +0000

    Updated manager field setting.

--- ReflectometryServer/ChannelAccess/pv_manager.py
+++ ReflectometryServer/ChannelAccess/pv_manager.py
@@ -110,9 +110,13 @@ QMIN_TEMPLATE = "{}:{{}}:{}".format(FOOTPRINT_PREFIX, "QMIN")
 QMAX_TEMPLATE = "{}:{{}}:{}".format(FOOTPRINT_PREFIX, "QMAX")
 FOOTPRINT_PREFIXES = [FP_SP_KEY, FP_SP_RBV_KEY, FP_RBV_KEY]
 
+MANAGER_FIELD = {"asg": "MANAGER"}
+STANDARD_FLOAT_PV_FIELDS_WITH_MANAGER = STANDARD_FLOAT_PV_FIELDS | MANAGER_FIELD
 PARAM_FIELDS_BINARY = {'type': 'enum', 'enums': ["NO", "YES"]}
+PARAM_FIELDS_BINARY_WITH_MANAGER = PARAM_FIELDS_BINARY | MANAGER_FIELD
 PARAM_IN_MODE = {'type': 'enum', 'enums': ["NO", "YES"]}
 PARAM_FIELDS_ACTION = {'type': 'int', 'count': 1, 'value': 0}
+PARAM_FIELDS_ACTION_WITH_MANAGER = PARAM_FIELDS_ACTION | MANAGER_FIELD
 STANDARD_2048_CHAR_WF_FIELDS = {'type': 'char', 'count': 2048, 'value': ""}
 STANDARD_STRING_FIELDS = {'type': 'string', 'value': ""}
 STANDARD_DISP_FIELDS = {'type': 'enum', 'enums': ["0", "1"], 'value': 0}
@@ -329,31 +333,25 @@ class PVManager:
                                  PvSort.RBV_AT_SP)
 
         # Locked PV
-        self._add_pv_with_fields(prepended_alias + LOCKED, param_name, PARAM_FIELDS_BINARY, description,
+        self._add_pv_with_fields(prepended_alias + LOCKED, param_name, PARAM_FIELDS_BINARY_WITH_MANAGER, description,
                                  PvSort.LOCKED)
 
         # Locked setpoint PV
-        self._add_pv_with_fields(prepended_alias + LOCKED_SP, param_name, PARAM_FIELDS_BINARY, description,
+        self._add_pv_with_fields(prepended_alias + LOCKED_SP, param_name, PARAM_FIELDS_BINARY_WITH_MANAGER, description,
                                  PvSort.LOCKED_SP)
 
         # define position at
         if parameter.define_current_value_as is not None:
-            align_fields = STANDARD_FLOAT_PV_FIELDS.copy()
-            align_fields["asg"] = "MANAGER"
-            action_fields = PARAM_FIELDS_ACTION.copy()
-            action_fields["asg"] = "MANAGER"
-            binary_fields = PARAM_FIELDS_BINARY.copy()
-            binary_fields["asg"] = "MANAGER"
-            self._add_pv_with_fields(prepended_alias + DEFINE_POS_SP, param_name, align_fields, description,
+            self._add_pv_with_fields(prepended_alias + DEFINE_POS_SP, param_name, STANDARD_FLOAT_PV_FIELDS_WITH_MANAGER, description,
                                      PvSort.DEFINE_POS_SP)
 
-            self._add_pv_with_fields(prepended_alias + DEFINE_POS_SET_AND_NO_ACTION, param_name, align_fields, description,
+            self._add_pv_with_fields(prepended_alias + DEFINE_POS_SET_AND_NO_ACTION, param_name, STANDARD_FLOAT_PV_FIELDS_WITH_MANAGER, description,
                                      PvSort.DEFINE_POS_SET_AND_NO_ACTION)
 
-            self._add_pv_with_fields(prepended_alias + DEFINE_POS_ACTION, param_name, action_fields, description,
+            self._add_pv_with_fields(prepended_alias + DEFINE_POS_ACTION, param_name, PARAM_FIELDS_ACTION_WITH_MANAGER, description,
                                      PvSort.DEFINE_POS_ACTION)
 
-            self._add_pv_with_fields(prepended_alias + DEFINE_POS_CHANGED, param_name, binary_fields, description,
+            self._add_pv_with_fields(prepended_alias + DEFINE_POS_CHANGED, param_name, PARAM_FIELDS_BINARY_WITH_MANAGER, description,
                                      PvSort.DEFINE_POS_CHANGED)
 
         # Engineering Unit
